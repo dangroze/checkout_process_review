ae960629b1251e0faabe49bff975645f8ac47b90
Dan Groze <dangroze@gmail.com>
2019-03-14 17:05:28 +0000

    :white_check_mark:Test- #checkout returns total cost of items

--- spec/supermarket_spec.rb
+++ spec/supermarket_spec.rb
@@ -3,7 +3,12 @@ require 'supermarket'
 RSpec.describe Supermarket do
   subject(:supermarket) { described_class.new }
   
-  it('Returns -1 for any illegal input') do
-    expect(supermarket.checkout('1')).to eq(-1)
+  describe('#checkout') do
+    it('Returns -1 for any illegal input') do
+      expect(supermarket.checkout('1-A')).to eq(-1)
+    end
+    it('Returns the total for valid items') do
+      expect(supermarket.checkout('ABCD')).to eq(115)
+    end
   end
 end
