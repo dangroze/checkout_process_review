4a3f7755bef27eb8fe801543fe641eb145fe4648
Dan Groze <dangroze@gmail.com>
2019-03-14 17:05:56 +0000

    :sparkles:Passed test- #checkout returns total cost of items

--- lib/supermarket.rb
+++ lib/supermarket.rb
@@ -1,8 +1,19 @@
 class Supermarket
 
+  def initialize
+    @price = {
+      A: 50,
+      B: 30,
+      C: 20,
+      D: 15
+    }
+  end
+
   def checkout(string)
     return -1 if (string =~ /\d/) || string.include?('-') || string.upcase != string
-
+    string.split('').reduce(0) do |sum, item|
+      sum += @price[item.to_sym]
+    end
   end
 
 end
